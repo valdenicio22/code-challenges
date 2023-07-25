d352bf1626e041495d25a2952f6068e154a44f79
valdenicio22 <valdenicio22@gmail.com>
2023-07-25 20:00:40 +0200

    problem solved

--- beecrowd/1005-Average1.java
+++ beecrowd/1005-Average1.java
@@ -13,20 +13,3 @@ public class Main {
     sc.close();
   }
 }
-
-import java.util.Scanner;
-
-public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-
-    float gradeA = sc.nextFloat();
-    float gradeB = sc.nextFloat();
-    float gradeC = sc.nextFloat();
-
-    double output = ((gradeA * 2) + (gradeB * 3) + (gradeB * 5)) / 10.0;
-    System.out.printf("MEDIA = %.1f\n", output);
-
-    sc.close();
-  }
-}
