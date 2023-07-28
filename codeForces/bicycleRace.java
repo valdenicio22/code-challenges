57ded706aac9c65a06d9fca1f744584cf8fbfa97
valdenicio22 <valdenicio22@gmail.com>
2023-07-28 10:48:53 +0200

    problem solved

---
+++ codeForces/bicycleRace.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class bicycleRace {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int first = 0;
+    int second = 1;
+
+    System.out.print(first);
+    for (int i = 0; i < n - 1; i++) {
+      System.out.print(" " + second);
+      int next = first + second;
+      first = second;
+      second = next;
+    }
+    System.out.println();
+    sc.close();
+  }
+}
