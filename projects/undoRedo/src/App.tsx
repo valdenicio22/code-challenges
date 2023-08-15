9331f776ba6ce6e379e8fba52e92f88956242b29
valdenicio22 <valdenicio22@gmail.com>
2023-08-15 13:10:30 +0200

    feat: solution with window

--- projects/undoRedo/src/App.tsx
+++ projects/undoRedo/src/App.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { MouseEvent, useEffect, useState } from 'react';
 import './App.css';
 
 interface ClickedStack {
@@ -11,12 +11,21 @@ function App() {
   const [clickedStack, setClickedStack] = useState<ClickedStack[]>([])
   const [poppedClicks, setPoppedClicks] = useState<ClickedStack[]>([])
 
-  const handleClick = (event: React.MouseEvent) => {
-    const { clientX, clientY } = event
-    setClickedStack((prev) => [...prev, { x: clientX, y: clientY }])
-  }
+  const handleClick = (ev: globalThis.MouseEvent): void => {
+    const { clientX, clientY } = ev;
+    setClickedStack((prev) => [...prev, { x: clientX, y: clientY }]);
+  };
 
-  const handleUndo = () => {
+  useEffect(() => {
+    window.addEventListener('click', handleClick);
+
+    return () => {
+      window.removeEventListener('click', handleClick);
+    };
+  }, []);
+
+  const handleUndo = (ev: MouseEvent<HTMLButtonElement>) => {
+    ev.stopPropagation();
     const shallowClickedStack = [...clickedStack];
     const removedClick = shallowClickedStack.pop();
     if (!removedClick) return;
@@ -24,7 +33,8 @@ function App() {
     setPoppedClicks((prev) => [...prev, removedClick]);
     return
   }
-  const handleRedo = () => {
+  const handleRedo = (ev: MouseEvent<HTMLButtonElement>) => {
+    ev.stopPropagation();
     const shallowPoppedClicks = [...poppedClicks];
     const removedClick = shallowPoppedClicks.pop();
     if (!removedClick) return;
@@ -37,11 +47,11 @@ function App() {
     <div>
       <button onClick={handleUndo} disabled={!clickedStack.length}>Undo</button>
       <button onClick={handleRedo} disabled={!poppedClicks.length}>Redo</button>
-      <div className='container' onClick={handleClick}>
-        {clickedStack.map((click, i) => (
-          <div key={i + 1} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '20px', height: '20px', borderRadius: '50%', background: 'black', color: 'white', position: 'absolute', top: click.y, left: click.x }}>{i + 1}</div>
-        ))}
-      </div>
+
+      {clickedStack.map((click, i) => (
+        <div key={i + 1} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '20px', height: '20px', borderRadius: '50%', background: 'black', color: 'white', position: 'absolute', top: click.y, left: click.x }}>{i + 1}</div>
+      ))}
+
     </div>
   )
 }
